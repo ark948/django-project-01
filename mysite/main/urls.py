8db8de7b0d2151fd7724a43e03e173b5b673d642
ark948 <armankarimi948@gmail.com>
2024-04-26 12:35:51 +0330

    Added a test form and route to send and get data

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -5,4 +5,6 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("send-data/", views.send_data_by_form, name="send_data"),
+    path("get-data/", views.get_data_by_form, name="get_data"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "main/index.html")
+
+def login(request):
+    return render(request, "main/login.html")
+
+def send_data_by_form(request):
+    return render(request, "main/form_test.html")
+
+def get_data_by_form(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        print(name)
+        
+    return render(request, "main/form_data.html", {"name": name})
